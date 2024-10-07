0936b6804923c08f0c55344284b0b91799c5cf9d
JW0203 <widwoo0203@gmail.com>
2024-10-07 14:39:20 -0700

    refactor: remove comments

--- js/signUp.js
+++ js/signUp.js
@@ -1,6 +1,5 @@
 import {apiUrl} from "./config.js";
 export function signUpForm() {
-	// document.getElementById('sign-up').addEventListener('click', function(){
 	const contentDiv = document.getElementById('load-content');
 
 	// load-content 내의 모든 기존 폼 제거
@@ -123,5 +122,4 @@ export function signUpForm() {
 			event.preventDefault();
 		}
 	});
-// })
 }
